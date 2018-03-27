afb179f17f84cf8a8b890b0fc5e2d89702b3262d
Małgorzata Nowak <goskanowak87@gmail.com>
2018-03-27 21:31:37 +0200

    Update files

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+// Użyj metody aby pobrać elementy ze strony i przypisz je do zmiennej.
 var buttonElement = document.getElementsByClassName('button');
+console.log(buttonElement);
 
+//Stwórz pętlę for, która wykona się tyle razy, ile jest elementów na stronie.
 
+var numberElement = buttonElement;
+var x = numberElement.length;
 
-//console.log(buttonElement);
+//Wewnątrz pętli wykonaj instrukcję, która wyświetli tekst zawarty w każdym z tych buttonów.
+    
+for (var a = 0; a < x; a++) {
+    var buttonText = buttonElement[a].innerText;
+    console.log(buttonText);
+    }
