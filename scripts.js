99c43d5fe1a49c0c78a5c2c2bec821c1f18425fd
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 02:48:14 +0200

    delete comment

--- scripts.js
+++ scripts.js
@@ -1,17 +1,10 @@
-// Użyj metody aby pobrać elementy ze strony i przypisz je do zmiennej.
-
 var buttonElement = document.getElementsByClassName('button');
 console.log(buttonElement);
 
-//Stwórz pętlę for, która wykona się tyle razy, ile jest elementów na stronie.
-
-var numberElement = buttonElement;
+// var numberElement = buttonElement;
 var x = numberElement.length;
-
-//Wewnątrz pętli wykonaj instrukcję, która wyświetli tekst zawarty w każdym z tych buttonów.
     
 for (var a = 0; a < x; a++) {
     var buttonText = buttonElement[a].innerText;
-    alert(buttonText);
     console.log(buttonText);
     }
