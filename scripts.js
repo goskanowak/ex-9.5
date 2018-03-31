e9fe6c63aed5948e7fba41848fa688750cbd3f86
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 02:58:26 +0200

    change variable

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 var buttonElement = document.getElementsByClassName('button');
 console.log(buttonElement);
 
-// var numberElement = buttonElement;
-var x = numberElement.length;
+var x = buttonElement.length;
     
 for (var a = 0; a < x; a++) {
     var buttonText = buttonElement[a].innerText;
